4454575a4310c6165601b6a65ec8798da950a977
yannick-aneo <ybley@aneo.fr>
2023-07-04 17:42:49 +0200

    feat: update task-grpc service for displaying tasks page

---
+++ src/app/tasks/components/tasks-list.component.ts
@@ -0,0 +1,41 @@
+import { AfterViewInit, Component, OnDestroy, inject } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { TaskGrpcService } from '../services/task-grpc.service';
+import { TaskSummary } from '@aneoconsultingfr/armonik.api.angular';
+
+@Component({
+  selector: 'app-tasks-list',
+  template: `
+   <h2> Essai Tasks List component </h2>
+  `,
+  styles: [`
+  `],
+  standalone: true,
+  providers: [
+    TaskGrpcService
+  ],
+})
+export class TasksListComponent implements AfterViewInit, OnDestroy {
+
+  taskList: TaskSummary[] | null = null;
+
+  loading = true;
+
+  #taskGrpcService = inject(TaskGrpcService);
+
+  subscriptions = new Subscription();
+
+  ngAfterViewInit(): void {
+    const subscription = this.#taskGrpcService.displayTasksList$()
+      .subscribe((response) => {
+        this.loading = false;
+        this.taskList = response.tasks ?? null;
+      });
+
+    this.subscriptions.add(subscription);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+}
--- src/app/tasks/services/task-grpc.service.ts
+++ src/app/tasks/services/task-grpc.service.ts
@@ -1,15 +1,52 @@
-import { CountTasksByStatusRequest, CountTasksByStatusResponse, TasksClient } from '@aneoconsultingfr/armonik.api.angular';
+import { CountTasksByStatusRequest, CountTasksByStatusResponse, TasksClient, ListTasksRequest, ListTasksResponse, TaskStatus, SessionField, SortDirection, TaskOptionField, TaskSummaryField, TaskField  } from '@aneoconsultingfr/armonik.api.angular';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class TaskGrpcService {
+
   constructor(
     private _tasksClient: TasksClient,
   ) {}
+
 
   countByStatu$(): Observable<CountTasksByStatusResponse> {
     const request = new CountTasksByStatusRequest();
     return this._tasksClient.countTasksByStatus(request);
   }
+  displayTasksList$(): Observable<ListTasksResponse> {
+    const tasksRequest = new ListTasksRequest({
+      page : 0, 
+      pageSize: 10,
+      filter: {
+        sessionId: TaskField.id,
+        status: [
+          TaskStatus.TASK_STATUS_CANCELLED,
+          TaskStatus.TASK_STATUS_CANCELLING,
+          TaskStatus.TASK_STATUS_COMPLETED,
+          TaskStatus.TASK_STATUS_DISPATCHED,
+          TaskStatus.TASK_STATUS_CREATING,
+          TaskStatus.TASK_STATUS_PROCESSED,
+          TaskStatus.TASK_STATUS_RETRIED,
+          TaskStatus.TASK_STATUS_SUBMITTED,
+          TaskStatus.TASK_STATUS_TIMEOUT, 
+          TaskStatus.TASK_STATUS_UNSPECIFIED,
+          TaskStatus.TASK_STATUS_ERROR 
+        ]
+      },
+      sort : {
+        direction: SortDirection.SORT_DIRECTION_ASC,
+        field: {
+          taskSummaryField: TaskSummaryField.TASK_SUMMARY_FIELD_STARTED_AT,
+          taskOptionField: TaskOptionField.TASK_OPTION_FIELD_UNSPECIFIED,
+          taskOptionGenericField: { field: 'h' }
+        }
+      },
+      withErrors: true
+    });
+    return this._tasksClient.listTasks(tasksRequest);
+  }
+
+  
 }
+
